9c2fa7bfbe5ca310c74f8f57f355cf4784d8d39b
nax ca <icavierescornejo13@gmail.com>
2020-10-23 21:21:21 -0300

    BoomComics: Creando pagina de inicio

--- BoomComics/BoomComics/settings.py
+++ BoomComics/BoomComics/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalogo',
+    'catalogo.apps.CatalogoConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BoomComics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- BoomComics/catalogo/views.py
+++ BoomComics/catalogo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from . models import Comic, Autor, Formato, Tipo
+from django.views import generic
 
 # Create your views here.
+
+def index(request):
+    comics = Comic.objects.all()
+
+    return render(
+        request, 
+        'index.html', 
+        context={'comics':comics}, 
+    )
